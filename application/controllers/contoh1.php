d13d3ee362ed7f3f2f82f25b334d137823a744e9
Otnielyn <otnielpro7@gmail.com>
2023-09-26 20:43:47 +0700

    menambahkan index.html,about.html,contact.html

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>" ;
+        echo "Nama saya Otniel Yusan Nalle
+            Saya tinggal di Kp.Gabus
+            Olahraga yang saya sukai adalah Futsal" ;
+    }
+}
