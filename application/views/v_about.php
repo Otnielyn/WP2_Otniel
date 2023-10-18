9dc50415d1032f1b46f0277e0f78187b4f4e03a3
Otnielyn <otnielpro7@gmail.com>
2023-10-18 22:32:53 +0700

    Menambahkan file p4

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Otniel Yusan</li>
+        <li>Nama Belakang : Nalle</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Kp. Gabus</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Futsal</li>
+        <li>Bulutangkis</li>
+    </ul>
+</section>
